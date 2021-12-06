3a42e049b8c9a9779140f7cca3be19e379187cb2
Msteve27 <msteve27@vols.utk.edu>
2021-12-06 10:11:54 -0600

    added index.js code

--- develop/index.js
+++ develop/index.js
@@ -1,13 +1,57 @@
 // TODO: Include packages needed for this application
+const inquirer = require('inquirer');
+const fs = require('fs');
+const generateMarkdown = require('./utils/generateMarkdown');
+
 
 // TODO: Create an array of questions for user input
-const questions = [];
+const questions = [
+    {
+        type:
+        name:
+        message:
+    },
+    {
+        type:
+        name:
+        message:
+    },
+    {
+        type:
+        name:
+        message:
+    },
+    {
+        type: 'checkbox',
+        name: 'license',
+        message: 'Please select a license',
+        choices: [
+            {name: 'MIT'},
+            {name: 'Apache'},
+            {name: 'BSD'},
+            {name: 'GNU'},
+            {name: 'ISC'},
+        ]
+    }
+];
 
 // TODO: Create a function to write README file
-function writeToFile(fileName, data) {}
+function writeToFile(fileName, data) {
+    return fs.writeFile(fileName, data, (err) => {
+        if (err) {
+            throw Error(err)
+        }
+    })
+}
 
 // TODO: Create a function to initialize app
-function init() {}
+function init() {
+    inquirer.prompt(questions)
+        .then(answers => {
+            const markDown = generateMarkdown(answers)
+            writeToFile('Hobbies.md', markDown)
+        })
+}
 
 // Function call to initialize app
 init();
