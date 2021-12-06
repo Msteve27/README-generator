67721a38008eebcf7dacd48696135c6f54bee7ff
Msteve27 <msteve27@vols.utk.edu>
2021-12-06 11:34:07 -0600

    added interpolations to generate Markdown

--- develop/index.js
+++ develop/index.js
@@ -1,25 +1,62 @@
 // TODO: Include packages needed for this application
 const inquirer = require('inquirer');
 const fs = require('fs');
-const generateMarkdown = require('./utils/generateMarkdown');
+const generateMarkdown = require('./utils/generateMarkdown.js');
 
 
 // TODO: Create an array of questions for user input
 const questions = [
     {
-        type:
-        name:
-        message:
+        type: 'input',
+        name: 'title',
+        message:'What is the title of your project? (required)',
+        validate: titleInput => {
+            if (titleInput) {
+                return true;
+            } else {
+                console.log('Please enter your project title.');
+                return false;
+            }
+        }
     },
     {
-        type:
-        name:
-        message:
+        type:'input',
+        name:'description',
+        message:'Give a description of your project: (required)',
+        validate: descriptionInput => {
+            if (descriptionInput) {
+                return true;
+            } else {
+                console.log('Please enter a description of your project.');
+                return false;
+            }
+        }
     },
     {
-        type:
-        name:
-        message:
+        type:'input',
+        name: 'installation',
+        message:'What are the steps required to install your project? Provide a step-by-step description of how to get the development environment running. (required)',
+        validate: installationInput => {
+            if (installationInput) {
+                return true;
+            } else {
+                console.log('Please enter installation instructions for your project.');
+                return false;
+            }
+        }
+    },
+    {
+        type:'input',
+        name: 'screenshot',
+        message:'Please upload screenshot to the images folder and copy relative path here. (required)',
+        validate: screenshotInput => {
+            if (screenshotInput) {
+                return true;
+            } else {
+                console.log('Please enter a path to the screenshot.');
+                return false;
+            }
+        }
     },
     {
         type: 'checkbox',
@@ -32,8 +69,36 @@ const questions = [
             {name: 'GNU'},
             {name: 'ISC'},
         ]
-    }
-];
+    },
+    {
+        type:'input',
+        name: 'username',
+        message:'What is your GitHub username?(required)',
+        validate: githubInput => {
+            if (githubInput) {
+                return true;
+            } else {
+                console.log('Please enter your GitHub username.');
+                return false;
+            }
+        }
+    },
+    {
+        type:'input',
+        name: 'email',
+        message:'What is your email?(required)',
+        validate: emailInput => {
+            if (emailInput) {
+                return true;
+            } else {
+                console.log('Please enter your email.');
+                return false;
+            }
+    
+      }
+    },
+]
+
 
 // TODO: Create a function to write README file
 function writeToFile(fileName, data) {
--- develop/utils/generateMarkdown.js
+++ develop/utils/generateMarkdown.js
@@ -1,19 +1,92 @@
 // TODO: Create a function that returns a license badge based on which license is passed in
 // If there is no license, return an empty string
-function renderLicenseBadge(license) {}
+function renderLicenseBadge(license) {
+    console.log('license: ', license);
+    switch (license) {
+      case 'MIT':
+        return `[![License: MIT](https://img.shields.io/badge/License-MIT-yellow.svg)](https://opensource.org/licenses/MIT)`
+      case 'Apache':
+        return `[![License](https://img.shields.io/badge/License-Apache_2.0-blue.svg)](https://opensource.org/licenses/Apache-2.0)`
+      case 'BSD':
+        return `[![License](https://img.shields.io/badge/License-BSD_3--Clause-blue.svg)](https://opensource.org/licenses/BSD-3-Clause)`
+      case 'GNU':
+        return `[![License: GPL v3](https://img.shields.io/badge/License-GPLv3-blue.svg)](https://www.gnu.org/licenses/gpl-3.0)`
+      case 'ISC':
+        return `[![License: ISC](https://img.shields.io/badge/License-ISC-blue.svg)](https://opensource.org/licenses/ISC)
+        `
+      default:
+        return `[![License: ISC](https://img.shields.io/badge/License-ISC-blue.svg)](https://opensource.org/licenses/ISC)`
+    }
+  ​
+  }
 
-// TODO: Create a function that returns the license link
-// If there is no license, return an empty string
-function renderLicenseLink(license) {}
+function renderProjectTitle(title) {
+    if (title === true) {
+        return `${title}`;
+    } 
+}
 
-// TODO: Create a function that returns the license section of README
-// If there is no license, return an empty string
-function renderLicenseSection(license) {}
+
+function renderProjectDescription(description) {
+    if (description === true) {
+        return `${description}`;
+    } 
+}
+
+function renderInstallation(installation) {
+    if (installation === true) {
+        return `${installation}`;
+    } 
+}
+
+function renderUsername(username) {
+    if (username === true) {
+        return `${installation}`;
+    } 
+}
+
+function renderEmail(email) {
+    if (email === true) {
+        return `${installation}`;
+    } 
+}
 
 // TODO: Create a function to generate markdown for README
 function generateMarkdown(data) {
-  return `# ${data.title}
-`;
+  console.log('data is ==>', data)
+  return `
+  # Project Title
+  ${renderProjectTitle(data.title)}
+
+  # Description
+  ${renderProjectDescription(data.description)}
+
+  # Table of Contents  
+  * [Installation](#Installation)\n
+  * [Usage](#Usage)\n
+  * [License](#License)\n
+  * [Contributing](#Contributing)\n
+  * [Tests](#Tests)\n
+  * [Questions](#Questions)\n
+
+  # Installation
+  ${renderInstallation(data.installation)}
+
+  # Usage
+  ${renderScreenshot(data.screenshot)}
+
+  # Liscense
+  This is licensed under: 
+  ${renderLicense(data.license)}
+
+  # Contributing
+
+  # Tests
+
+  # Questions
+  https://github.com/${renderUsername(data.username)}
+  ${renderEmail(data.email)}
+  `;
 }
 
 module.exports = generateMarkdown;
